c121de12c929860b73b31d0f7554ce52f58e5855
KimByeolhee <qufgml0216@naver.com>
2022-07-12 11:35:33 +0900

    [Feat]: url로부터 알약 img 다운로드

---
+++ data/README.md
@@ -0,0 +1,9 @@
+## 의약품안전나라 의약품 낱알식별 데이터셋
+
+- [pills_data.csv](https://nedrug.mfds.go.kr/pbp/CCBGA01/getItem?totalPages=4&limit=10&page=2&&openDataInfoSeq=11)
+
+url로부터 알약 이미지 다운로드
+
+```python
+$ python download_url_to_img.py
+```
---
+++ data/download_url_to_img.py
@@ -0,0 +1,27 @@
+import os
+import urllib.request
+import pandas as pd
+import re
+
+def download_image(img_url, pill_name, save_path):
+  result_url = './img/'
+  os.mkdir(result_url + pill_name)
+  urllib.request.urlretrieve(img_url, result_url + save_path + '.jpg')
+
+if __name__ == "__main__":
+    data = pd.read_csv("./pills_data.csv")
+    image_url = pd.DataFrame(data['큰제품이미지'])
+
+    for i,row in image_url.iterrows():
+        if 'https://' in row['큰제품이미지']:
+            # test 중 임시 10개
+            if i == 10:
+                break
+
+            try:
+                pill_name = re.split('[/,-,(,).:]', data['품목명'][i])[0]
+                save_path = f'{pill_name}/{pill_name}_1'
+
+                download_image(row['큰제품이미지'], pill_name, save_path)
+            except:
+                pass
